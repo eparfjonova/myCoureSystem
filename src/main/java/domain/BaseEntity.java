0b5177cac82a7fb56c122bf8d42562e67fe08cd5
eparfjonova <eparfjonova@tsn.at>
2024-12-03 11:38:59 +0100

    5. Video begonnen (Stunde hat geendet bin gerade bei 4:29)

---
+++ src/main/java/domain/BaseEntity.java
@@ -0,0 +1,7 @@
+package domain;
+
+public abstract class BaseEntity {
+
+    //keine Instanzen von dieser Klasse können direkt erstellt werden, aber wir können Funktionen und Variablen implementeiren die in anderen Klassen verwendet werden
+    //ich bin gerade Video 5. 4:29
+}
