1f25398e3cc5b8d1c95403389f11cb6c2d085d55
eparfjonova <eparfjonova@tsn.at>
2024-12-03 11:30:57 +0100

    4. Video fertig nachprogrammiert (Kommandozeilenmenue implementiert)

--- src/main/java/at/hakimst/Main.java
+++ src/main/java/at/hakimst/Main.java
@@ -1,18 +1,25 @@
 package at.hakimst;
 
 import at.hakimst.dataaccess.MySqlDatabaseConnection;
+import at.hakimst.ui.Cli;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+
+
+        Cli myCli = new Cli();
+        myCli.start();
 
 
         try {
             Connection myConnection = MySqlDatabaseConnection.getConnection("jdbc:mysql://127.0.0.1:3306/kurssystem", "root","");
             System.out.println("Verbundung aufgebaut");
+
+
+
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } catch (SQLException e) {
--- src/main/java/at/hakimst/ui/Cli.java
+++ src/main/java/at/hakimst/ui/Cli.java
@@ -1,5 +1,6 @@
 package at.hakimst.ui;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Cli {
@@ -14,11 +15,37 @@ public class Cli {
     public void start(){
 
         String input = "-";
-        while(input.equals("x")){
+        while(!input.equals("x")){
 
             showMenue();
             input = scan.nextLine();
-
+            switch(input) {
+                case "1":
+                    System.out.println("Kurseingabe");
+                    break;
+                case "2":
+                    System.out.println("Alle Kurse anzeigen");
+                    break;
+                case "3":
+                    break;
+                case "x":
+                    System.out.println("Auf Wiedersehen");
+                    break;
+                default:
+                    inputError();
+                    break;
+            }
         }
+        scan.close();
+    }
+
+    private void showMenue(){
+        System.out.println("--- KURSMANAGEMENT ---");
+        System.out.println("(1) Kurs eingeben \t(2) Alle Kurse anzeigen \t(3)");
+        System.out.println("(x) Ende für das Komandozeilenmenue");
+    }
+
+    private void inputError(){
+        System.out.println("Bitte nur die Zalen der Menüauswahl eingeben");
     }
 }
